7a89fa6ecb8cc46e75aa4b05d9e4d09d60c298a9
Abhishek Patel <abhishek26@ufl.edu>
2016-08-01 18:33:28 -0400

    Updated Welcome.java file

--- HelloWorld/src/Welcome.java
+++ HelloWorld/src/Welcome.java
@@ -4,15 +4,38 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		
+		boolean keep = true;
+		
 		Scanner s = new Scanner(System.in);
 		
-		System.out.print("Type any integer: ");
+		do{
+			
+			int randNum = (int)(Math.random()*100)+1;
+			
+			System.out.print("Type any integer from 1-100: ");
+			
+			int input = s.nextInt();
+			
+			int diff = Math.abs(input-randNum);
+			
+			if(diff != 0){
+				
+				System.out.println("Your chosen number is " + diff + " away from the randomly generated integer value of " + randNum +"!");
+				
+			}
+			
+			else{
+					
+				System.out.println("Congrats. Your integer matches the randomly generated integer " + randNum + "!!!");
+				
+				keep = false;
+			}
+			
+		}while(keep);
 		
-		int input = s.nextInt();
-		
-		System.out.println("Your favorite number appears to be " + input);
 		
 		s.close();
+	
 		
 	}
 
